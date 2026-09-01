3efb103558cd1308e72c55f248b29177838e4fb1
Sajjad Ali <sajjadramzan1211@gmail.com>
2026-09-01 19:43:00 +0500

    fix(db): drizzle config fixed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,21 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const isProd = process.env.DB_ENV === 'production';
+
 export default defineConfig({
   out: './drizzle',
   schema: './server/db/schema.ts',
-  dialect: 'sqlite',
-  dbCredentials: {
-    url: 'file:local.db',
-  },
+  ...(isProd
+    ? {
+        dialect: 'turso',
+        dbCredentials: {
+          url: process.env.TURSO_DATABASE_URL!,
+          authToken: process.env.TURSO_AUTH_TOKEN,
+        },
+      }
+    : {
+        dialect: 'sqlite',
+        dbCredentials: { url: 'file:local.db' },
+      }),
 });
